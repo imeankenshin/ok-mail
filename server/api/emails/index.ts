b1abd58106147423e347d58d270af8f44e08d4d3
imeankenshin <omurakenshin@gmail.com>
2025-02-27 19:03:21 +0900

    isReadがバグってるよーーーー！！！！！

--- server/api/emails/index.ts
+++ server/api/emails/index.ts
@@ -27,7 +27,7 @@ export default defineVerifiedOnlyEventHandler<
           userId: "me",
           id: message.id as string,
           format: "metadata",
-          metadataHeaders: ["From", "Subject", "Date", "LabelIds"],
+          metadataHeaders: ["From", "Subject", "Date"],
         });
 
         if (!email.data.id) {
@@ -38,7 +38,7 @@ export default defineVerifiedOnlyEventHandler<
         const subject = headers?.find((h) => h.name === "Subject")?.value;
         const from = headers?.find((h) => h.name === "From")?.value;
         const date = headers?.find((h) => h.name === "Date")?.value;
-        const isRead = !headers?.find((h) => h.name === "LabelIds")?.value?.includes("UNREAD");
+        const isRead = !email.data.labelIds?.includes("UNREAD");
 
         const emailData: Email = {
           id: email.data.id,
