992b131abc43bb752f48f8a2b05942ca5f2e2232
imeankenshin <omurakenshin@gmail.com>
2025-02-27 18:20:59 +0900

    サイドバーをコンポーネント化

--- app.vue
+++ app.vue
@@ -1,47 +1,19 @@
+<script setup lang="ts">
+import { RefreshCw, Plus } from "lucide-vue-next";
+
+const refreshEmails = () => {
+  // メール更新ロジックをここに実装
+  console.log("メールを更新中...");
+};
+
+const handleLogout = () => {
+  signOut();
+};
+</script>
+
 <template>
   <UiSidebarProvider>
-    <UiSidebar collapsible="icon">
-      <UiSidebarHeader />
-      <UiSidebarContent>
-        <UiSidebarGroup>
-          <UiSidebarGroupContent>
-            <UiSidebarMenu>
-              <UiSidebarMenuItem>
-                <UiSidebarMenuButton tooltip="受信トレイ" as-child>
-                  <NuxtLink to="/">
-                    <Mail class="mr-2 h-4 w-4" />
-                    受信トレイ
-                  </NuxtLink>
-                </UiSidebarMenuButton>
-              </UiSidebarMenuItem>
-              <UiSidebarMenuItem>
-                <UiSidebarMenuButton tooltip="送信済み" as-child>
-                  <NuxtLink to="/sent">
-                    <Send class="mr-2 h-4 w-4" />
-                    送信済み
-                  </NuxtLink>
-                </UiSidebarMenuButton>
-              </UiSidebarMenuItem>
-              <UiSidebarMenuItem>
-                <UiSidebarMenuButton tooltip="下書き" as-child>
-                  <NuxtLink to="/drafts">
-                    <FileText class="mr-2 h-4 w-4" />
-                    下書き
-                  </NuxtLink>
-                </UiSidebarMenuButton>
-              </UiSidebarMenuItem>
-              <UiSidebarMenuItem>
-                <UiSidebarMenuButton tooltip="ログアウト" @click="signOut()">
-                  <LogOut class="mr-2 h-4 w-4" />
-                  ログアウト
-                </UiSidebarMenuButton>
-              </UiSidebarMenuItem>
-            </UiSidebarMenu>
-          </UiSidebarGroupContent>
-        </UiSidebarGroup>
-      </UiSidebarContent>
-      <UiSidebarRail />
-    </UiSidebar>
+    <AppSidebar @logout="handleLogout" />
     <div class="min-h-screen bg-background w-full">
       <header class="border-b sticky inset-0 bg-background">
         <div class="flex h-16 items-center px-4">
@@ -66,12 +38,3 @@
     </div>
   </UiSidebarProvider>
 </template>
-
-<script setup>
-import { Mail, Send, FileText, LogOut, RefreshCw, Plus } from "lucide-vue-next";
-
-const refreshEmails = () => {
-  // メール更新ロジックをここに実装
-  console.log("メールを更新中...");
-};
-</script>
---
+++ components/app/AppSidebar.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { Mail, Send, FileText, LogOut } from "lucide-vue-next";
+const emit = defineEmits(["logout"]);
+
+const signOut = () => {
+  emit("logout");
+};
+</script>
+
+<template>
+  <UiSidebar collapsible="icon">
+    <UiSidebarHeader />
+    <UiSidebarContent>
+      <UiSidebarGroup>
+        <UiSidebarGroupContent>
+          <UiSidebarMenu>
+            <UiSidebarMenuItem>
+              <UiSidebarMenuButton tooltip="受信トレイ" as-child>
+                <NuxtLink to="/">
+                  <Mail class="mr-2 h-4 w-4" />
+                  受信トレイ
+                </NuxtLink>
+              </UiSidebarMenuButton>
+            </UiSidebarMenuItem>
+            <UiSidebarMenuItem>
+              <UiSidebarMenuButton tooltip="送信済み" as-child>
+                <NuxtLink to="/sent">
+                  <Send class="mr-2 h-4 w-4" />
+                  送信済み
+                </NuxtLink>
+              </UiSidebarMenuButton>
+            </UiSidebarMenuItem>
+            <UiSidebarMenuItem>
+              <UiSidebarMenuButton tooltip="下書き" as-child>
+                <NuxtLink to="/drafts">
+                  <FileText class="mr-2 h-4 w-4" />
+                  下書き
+                </NuxtLink>
+              </UiSidebarMenuButton>
+            </UiSidebarMenuItem>
+            <UiSidebarMenuItem>
+              <UiSidebarMenuButton tooltip="ログアウト" @click="signOut()">
+                <LogOut class="mr-2 h-4 w-4" />
+                ログアウト
+              </UiSidebarMenuButton>
+            </UiSidebarMenuItem>
+          </UiSidebarMenu>
+        </UiSidebarGroupContent>
+      </UiSidebarGroup>
+    </UiSidebarContent>
+    <UiSidebarRail />
+  </UiSidebar>
+</template>
