0dca15851c850d3ed6fed34ef9085b14af490a78
imeankenshin <omurakenshin@gmail.com>
2025-03-01 19:18:44 +0900

    バリデーションを実装

--- server/api/emails/send.ts
+++ server/api/emails/send.ts
@@ -2,26 +2,34 @@ import { google } from "googleapis";
 import { readBody } from "h3";
 import { tryCatch } from "#shared/utils/error";
 import { defineVerifiedOnlyEventHandler } from "~/server/utils/handler";
+import { object, string, pipe, email, minLength, safeParse} from "valibot";
+
+const sendEmailRequestSchema = object({
+  to: pipe(string(), email()),
+  subject: pipe(string(), minLength(1)),
+  body: pipe(string(), minLength(1)),
+});
 
 export default defineVerifiedOnlyEventHandler(async (event) => {
-  const [{ to, subject, body }, readBodyError] = await tryCatch(() => readBody(event));
+  const body = safeParse(await readBody(event), sendEmailRequestSchema);
 
-  if (readBodyError) {
+  if (!body.success) {
     throw createError({
       statusCode: 400,
       message: "Invalid body",
+      data: body.issues,
     });
   }
 
   const gmail = google.gmail({ version: "v1", auth: event.context.oAuth2Client });
-  const utf8Subject = `=?utf-8?B?${Buffer.from(subject).toString("base64")}?=`;
+  const utf8Subject = `=?utf-8?B?${Buffer.from(body.output.subject).toString("base64")}?=`;
   const messageParts = [
-    `To: ${to}`,
+    `To: ${body.output.to}`,
     "Content-Type: text/plain; charset=utf-8",
     "MIME-Version: 1.0",
     `Subject: ${utf8Subject}`,
     "",
-    body,
+    body.output.body,
   ];
   const message = messageParts.join("\n");
   const encodedMessage = Buffer.from(message)
